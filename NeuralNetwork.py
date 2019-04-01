cf2fe52e3948a6d5a255a5a72308a0595a43226d
EDDxample <EDDxample@gmail.com>
2019-04-01 15:21:13 -0400

    define forward pass + fix random range

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,19 +13,30 @@ def COST(Ypredicted, Yreal, isDerivative=False):
 
 class Layer():
     def __init__(self, n_neurons, n_prev_neurons, activation=SIGMOID):
-        self.W = np.random.rand(n_neurons, n_prev_neurons)
-        self.B = np.random.rand(n_neurons, 1)
+        self.W = np.random.rand(n_neurons, n_prev_neurons) *2-1
+        self.B = np.random.rand(n_neurons, 1)              *2-1
         
         self.activation = activation
 
 class NeuralNetwork():
     def __init__(self, topology):
+        self.isTraining = False
         self.layers = []
         for neurons, prev_neurons in zip(topology[1:],topology[:-1]):
             self.layers.append(Layer(neurons, prev_neurons))
     
     def forward_pass(self, input):
-        pass
+        activatedLayers = [input]
+
+        for layer in self.layers:
+            z = layer.W @ activatedLayers[-1] + layer.B
+            a = layer.activation(z)
+            activatedLayers.append(a)
+        
+        if self.isTraining:
+            return activatedLayers
+        return activatedLayers[-1]
+
     def train(self, test_input, test_output, learing_rate=0.5, cost_function=COST):
         pass
 
