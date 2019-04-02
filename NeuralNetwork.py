a84ebb71737e410fb2deb6385d1854552848d9e7
EDDxample <EDDxample@gmail.com>
2019-04-01 21:17:24 -0400

    add back propagation \o/

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -27,7 +27,7 @@ class NeuralNetwork():
     def forward_pass(self, input, isTraining=False):
         activatedLayers = [input]
 
-        for layer in self.layers:
+        for layer in self:
             z = layer.W @ activatedLayers[-1] + layer.B
             a = layer.activation(z)
             activatedLayers.append(a)
@@ -35,9 +35,30 @@ class NeuralNetwork():
         if isTraining:
             return activatedLayers
         return activatedLayers[-1]
+    
+    def backprop(self, dataset_input, dataset_output, learing_rate=0.5, cost_func=COST):
+        layer_outputs = self.forward_pass(dataset_input, True)
 
-    def train(self, test_input, test_output, learing_rate=0.5, cost_function=COST):
-        pass
+        deltas = [] # layer errors
+
+        for L in reversed(range(len(self))): # from layers.length-1 to 0
+            a = layer_outputs[L + 1]
+            a_prev = layer_outputs[L]
+            activ_a = self[L].activation(a, True)
+            
+            if L == len(self) - 1:
+                # last layer's error
+                deltas.insert(0, cost_func(a, dataset_output, True) * activ_a)
+            else:
+                # previous layer's error
+                deltas.insert(0, nextW @ deltas[0] * activ_a)
+            
+            nextW = self[L].W.T
+
+            # fix layer components
+            self[L].B -= np.mean(deltas[0], axis=1, keepdims=True) * learing_rate
+            self[L].W -= deltas[0] @ a_prev.T * learing_rate
+        
 
     def __len__(self): return len(self.layers)
     def __getitem__(self, index): return self.layers[index]
